40323c27b89b1f5bda194c0e037cea41d104850c
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2020-02-04 20:47:21 -0800

    Updates

---
+++ precache-manifest.c83d2a1b793e0423a2445508bd720720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11f5b1ab6c33f2f5d6d70bac93e11a9",
+    "url": "/nutritionapp/index.html"
+  },
+  {
+    "revision": "8aa96644c5cf71b8a97f",
+    "url": "/nutritionapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/nutritionapp/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nutritionapp/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa96644c5cf71b8a97f",
+    "url": "/nutritionapp/static/js/main.586d6bdf.chunk.js"
+  },
+  {
+    "revision": "4a3d9a0a801c62b69921",
+    "url": "/nutritionapp/static/js/runtime-main.8b9160cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nutritionapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
